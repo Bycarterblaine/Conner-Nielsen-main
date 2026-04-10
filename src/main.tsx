713e7d796380bcda4c4260f7020e27929ec2524a
Bycarterblaine <116913229+Bycarterblaine@users.noreply.github.com>
2026-04-10 16:49:21 -0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { BrowserRouter } from "react-router-dom";
+
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
