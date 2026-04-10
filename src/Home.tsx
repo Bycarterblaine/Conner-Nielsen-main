e6ed10d8f2dd268db27a2535316f6949c7fe83d0
Bycarterblaine <116913229+Bycarterblaine@users.noreply.github.com>
2026-04-10 17:00:59 -0500

    Create Home.tsx

---
+++ src/Home.tsx
@@ -0,0 +1,14 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import BlogPost from "./pages/BlogPost";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/blog/:slug" element={<BlogPost />} />
+    </Routes>
+  );
+}
+
+export default App;
