7e4acbb1188075238e81ca24096eccf2416c6d1b
Bycarterblaine <116913229+Bycarterblaine@users.noreply.github.com>
2026-04-10 17:58:09 -0500

    Update main.tsx

--- main.tsx
+++ main.tsx
@@ -1,17 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import { BrowserRouter } from "react-router-dom";
-
-root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-);
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './index.css';
+import App from './App';
+import BlogPost from './pages/BlogPost';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/blog/:slug" element={<BlogPost />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
